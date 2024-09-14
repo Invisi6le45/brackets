f62a5ed778b5df68fa47cb693c1127433b38d4a2
Nikolai <n_prusov@inbox.ru>
2024-09-14 21:18:18 +0300

    16/20

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
   console.log (`Входящий массив равен ${str}`);
   
-/*   console.log (`Массив скобок равен ${bracketsConfig}`);
-  console.log (`Длина входящего массива скобок ${bracketsConfig[0].length}`); */
+  console.log (`Массив скобок равен ${bracketsConfig}`);
+  /* console.log (`Длина входящего массива скобок ${bracketsConfig[0].length}`); */
 
 
   const rezult = [];
   let item = '';
-  let open = '';
-  let close = '';
+  let open = [];
+  let close = [];
   let rezultName = true;
 
  /*  console.log (`Исходящий массив равен ${typeof(rezult)}`); */
@@ -16,38 +16,38 @@ module.exports = function check(str, bracketsConfig) {
 
 
 
-  function openClosed (bracketsConfig, group) {
-    for (let j = 0; j <= bracketsConfig[group].length - 1; j++) {
-      if ( j === 0) {
-        open = bracketsConfig[group][j]; 
-      } else {
-        close = bracketsConfig[group][j];
-      }
-    }
-    
+  function openClosed (bracketsConfig) {
 
-    console.log (`Открытая скобка = ${open}`)
-    console.log (`Закрытая скобка = ${close}`);
-    
-  }
- 
-  
-  for (let i = 0; i <= bracketsConfig.length - 1; i++) {
-    
-    openClosed(bracketsConfig, i);
-    for (let z = 0; z <= str.length - 1; z++) {
-      if (str[z] === open) {
-        item = `${str[z]}`;
-        rezult.push(item);
-      } else {
-        if (str[z] === close && rezult.length !== 0) {
-          rezult.pop()
+
+    for (let i = 0; i <= bracketsConfig.length - 1; i++) {
+      for (let j = 0; j <= bracketsConfig[i].length - 1; j++) {
+        if ( j === 0) {
+          open.push(`${bracketsConfig[i][j]}`);
         } else {
-          rezultName = false;
+          close.push(`${bracketsConfig[i][j]}`);
         }
       }
     }
+    console.log (`Массив открытых скобок = ${open}`)
+    console.log (`Массив закрытых скобок = ${close}`);
   }
+ 
+  openClosed(bracketsConfig);
+
+    
+  for (let z = 0; z <= str.length - 1; z++) {
+    if ( open.includes(str[z])) {
+      item = `${str[z]}`;
+      rezult.push(item);
+    } else {
+      if ( close.includes(str[z]) && rezult.length !== 0) {
+        rezult.pop()
+      } else {
+        rezultName = false;
+      }
+    }
+  }
+  
 
 
   console.log (`Выходной массив = ${rezult}`);
