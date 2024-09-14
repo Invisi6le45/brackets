bc05145a9a8cc5d8225bf090c10fe0d12e78b087
Nikolai <n_prusov@inbox.ru>
2024-09-14 20:40:00 +0300

    12/8

--- src/index.js
+++ src/index.js
@@ -1,3 +1,68 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  console.log (`Входящий массив равен ${str}`);
+  
+/*   console.log (`Массив скобок равен ${bracketsConfig}`);
+  console.log (`Длина входящего массива скобок ${bracketsConfig[0].length}`); */
+
+
+  const rezult = [];
+  let item = '';
+  let open = '';
+  let close = '';
+  let rezultName = true;
+
+ /*  console.log (`Исходящий массив равен ${typeof(rezult)}`); */
+
+
+
+
+  function openClosed (bracketsConfig, group) {
+    for (let j = 0; j <= bracketsConfig[group].length - 1; j++) {
+      if ( j === 0) {
+        open = bracketsConfig[group][j]; 
+      } else {
+        close = bracketsConfig[group][j];
+      }
+    }
+    
+
+    console.log (`Открытая скобка = ${open}`)
+    console.log (`Закрытая скобка = ${close}`);
+    
+  }
+ 
+  
+  for (let i = 0; i <= bracketsConfig.length - 1; i++) {
+    
+    openClosed(bracketsConfig, i);
+    for (let z = 0; z <= str.length - 1; z++) {
+
+      if (str[z] === open) {
+        item = `${str[z]}`;
+        rezult.push(item);
+      } else {
+        if (str[z] === close && rezult.length !== 0) {
+          rezult.pop()
+        } else {
+          rezultName = false;
+        }
+      }
+
+
+    }
+  }
+
+  console.log (`Выходной массив = ${rezult}`);
+
+  if (rezult.length === 0) {
+    rezultName = true;
+  } else {
+    rezultName = false;
+  }
+
+ return rezultName;
 }
+
+
+
+
